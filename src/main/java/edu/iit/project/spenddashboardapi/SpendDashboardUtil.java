78f460f84cee0745b045585d9efb4ead65910d9f
John Coyne <john.coyne@gmail.com>
2023-11-06 21:31:32 -0600

    Implement method

--- src/main/java/edu/iit/project/spenddashboardapi/SpendDashboardUtil.java
+++ src/main/java/edu/iit/project/spenddashboardapi/SpendDashboardUtil.java
@@ -5,7 +5,10 @@ import java.util.List;
 public class SpendDashboardUtil {
     
     public static Double calculateTotal(List<TransactionOutput> transactionList) {
-        
-        return null;
+        Double total = 0.0;
+        for (TransactionOutput transaction : transactionList) {
+            total += Double.parseDouble(transaction.getTransactionAmount());
+        }
+        return total;
     }
 }
